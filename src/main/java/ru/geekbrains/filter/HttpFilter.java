19b9d03c79fb07f16aabad42881f2575b2f6fbd1
chendyrevanastya <nat-217@mail.ru>
2020-03-23 23:20:52 +0500

    Lesson2.1

---
+++ src/main/java/ru/geekbrains/filter/HttpFilter.java
@@ -0,0 +1,28 @@
+package ru.geekbrains.filter;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+
+@WebFilter(filterName = "HeaderFooter", urlPatterns = "/*")
+public class HttpFilter implements Filter {
+    private FilterConfig filterConfig;
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        this.filterConfig = filterConfig;
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        response.getWriter().println("<h1>HEADER</h1>");
+        chain.doFilter(request, response);
+        response.getWriter().println("<h1>FOOTER</h1>");
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
